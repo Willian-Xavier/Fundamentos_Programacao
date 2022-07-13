addcc61f1178f377abea32af1d964ea997410446
Willian Xavier <willian203@hotmail.com>
2022-07-13 20:45:26 -0300

    concluída

--- src/Questao_09.java
+++ src/Questao_09.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Questao_09 {
+import java.util.Scanner;
+
+public class Questao_09 {
+    public static void main(String[] args) {
+        double b, alt, area;
+
+        Scanner dado = new Scanner(System.in);
+
+        System.out.print("Informe o tamanho da base do triângulo: ");
+        b = dado.nextDouble();
+        System.out.print("Informe a altura do triângulo: ");
+        alt= dado.nextDouble();
+
+        area = (b*alt)/2;
+
+        System.out.printf("%s%.2f","A área do triângulo é igual a ",area);
+    }
 }
